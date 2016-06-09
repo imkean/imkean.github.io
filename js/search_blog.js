b34d881a464a222b042ee4dd7fcecc432dd92fb5
iKeansMac <imkean@163.com>
2016-06-10 02:04:07 +0800

    simplified the search-blog js module

--- js/search_blog.js
+++ js/search_blog.js
@@ -1,87 +1,36 @@
- $(document).ready(function () {
-        var time1 = 0;
-        var show = false;
-        var names = new Array(); //文章名字等
-        var urls = new Array(); //文章地址
-        $(document).keyup(function (e) {
-            e = e || window.event;
-            var time2 = new Date().getTime();
-            if (e.keyCode == 17) {
-                var gap = time2 - time1;
-                time1 = time2;
-                if (gap < 500) {
-                    if (show) {
-                        //$(".search-tool").css("display", "none");
-                        show = false;
-                    } else {
-                        $(".search-tool").css("display", "block");
-                        show = true;
-                        $("#search-text").val("");
-                        $("#search-text").focus();
-                    }
-                    time1 = 0;
-                }
-			}else if(e.keyCode == 27){
-                    //$(".search-tool").css("display", "none");
-                    show = false;
-                    time1 = 0;
-                }
-        });
+$(document).ready(function () {
+  var names = new Array(); //文章名字等
+  var urls = new Array(); //文章地址
 
- 		$("#search-text").keyup(function (e) {
-            e = e || window.event;
-            var time2 = new Date().getTime();
-            if (e.keyCode == 17) {
-                var gap = time2 - time1;
-                time1 = time2;
-                if (gap < 500) {
-                    if (show) {
-                        //$(".search-tool").css("display", "none");
-                        show = false;
-                    } else {
-                        //$(".search-tool").css("display", "block");
-                        show = true;
-                        $("#search-text").val("");
-                        $("#search-text").focus();
-                    }
-                    time1 = 0;
-                }
-            }
-        });
+  $("#search-btn").click(function(){
+    //$("#search-text").val("");
+    $("#search-text").focus();
+  });
 
-        $("#close-btn").click(function () {
-            //$(".search-tool").css("display", "none");
-            show = false;
-            time1 = 0;
-        });
+  //$("#search-text").blur(function(){
+  //$("ul.typeahead.dropdown-menu").css("display", "none");
+  //})
 
-        $("#search-btn").click(function(){
-            //$(".search-tool").css("display", "block");
-            show = true;
-            //$("#search-text").val("");
-            $("#search-text").focus();
-            time1 = 0;
-        });
+  $.getJSON("/search/search_blog_data.json").done(function (data) {
+    if (data.code == 0) {
+      for (var index in data.data) {
+        var item = data.data[index];
+        names.push(item.title);
+        urls.push(item.url);
+      }
 
-        $.getJSON("/search/search_blog_data.json").done(function (data) {
-            if (data.code == 0) {
-                for (var index in data.data) {
-                    var item = data.data[index];
-                    names.push(item.title);
-                    urls.push(item.url);
-                }
+      $("#search-text").typeahead({
+        source: names,
+        items: 8, /*max matched item to show*/
+        minLength: 3,/*min matched sub-string*/
 
-                $("#search-text").typeahead({
-                    source: names,
+        afterSelect: function (item) {
+          //$("ul.typeahead.dropdown-menu").css("display", "none");
+          window.location.href = ""+(urls[names.indexOf(item)]);
+          return item;
+        }
+      });
+    }
+  });
 
-                    afterSelect: function (item) {
-						//$(".search-tool").css("display", "none");
-                        show = false;
-                        window.location.href = ""+(urls[names.indexOf(item)]);
-                        return item;
-                    }
-                });
-            }
-        });
-
-    });
+});
