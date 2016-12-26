502dca652d38219b6378c41afbc3d65adedf8c7b
iKeansMac <imkean@163.com>
2016-12-27 00:24:00 +0800

    ..

--- js/typed_setting.js
+++ js/typed_setting.js
@@ -2,7 +2,7 @@ $(function(){
     $("#typed").typed({
     // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
         stringsElement: $('#typed-strings'),
-        typeSpeed: 60,
+        typeSpeed: 150,
         backDelay: 2000,
         loop: true,
         contentType: 'html', // or text
